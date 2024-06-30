d6e6ca2ab49d0a639fd38cf02e0b1acfe959c3d5
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 01:12:59 +0800

    Add solution to 04_removeFromArray
    Used functional style of programming to solve
    the problem.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    return arr.filter(value => !elements.includes(value));
 };
 
 // Do not edit below this line
