b858b543b23358a4c8dfe915183866d6d118ac77
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 01:05:16 +0800

    Add solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    for (let i = 0; i < string.length; i++) {
+        output += string[string.length - 1 - i];
+    }
+    return output;
 };
 
 // Do not edit below this line
