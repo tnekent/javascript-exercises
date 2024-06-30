36095604a998a2a6289a5226aa7f525275134e88
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 01:23:54 +0800

    Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(bound1, bound2) {
 
+    if (bound1 < 0 || bound2 < 0 ||
+        typeof bound1 != "number" || 
+        typeof bound2 != "number") {
+            return "ERROR";
+        }
+
+    let acc = 0,
+        from = Math.min(bound1, bound2),
+        to = Math.max(bound1, bound2);
+
+    for (let i = from; i <= to; i++) {
+        acc += i;
+    }
+    return acc;
 };
 
 // Do not edit below this line
