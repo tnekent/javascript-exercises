a9bf05bbc26d56505b89832088006b55d1eb88fb
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 21:31:43 +0800

    Add solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Years divisible by 100 are leap years ONLY if they are divisible by 400
+    // Without this condition, years like 1900 becomes true even though they're
+    // actually not.
+    if (year % 100 === 0)
+        return year % 400 === 0;
+    else return year % 4 === 0;
 };
 
 // Do not edit below this line
