4ff4bf7bb6f0d15cd7b74546baed244ec5ad3c84
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 21:45:09 +0800

    Add solution to 07_tempConversion
    Used `Number()` to convert from string of `toFixed()` back to type
    number since this is much easier to round with precision.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = 5 / 9 * (tempF - 32);
+  return roundToOneDecimal(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (9 / 5 * tempC) + 32;
+  return roundToOneDecimal(tempF);
 };
+
+const roundToOneDecimal = function(num) {
+  return Number(num.toFixed(1));
+}
 
 // Do not edit below this line
 module.exports = {
