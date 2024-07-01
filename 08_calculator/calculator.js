0f18c479179d857b20086c6cee0f5eaf3f1b58e2
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-01 21:59:52 +0800

    Add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numberList) {
+	return numberList.reduce((acc, val) => acc + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberList) {
+  return numberList.reduce((acc, val) => acc * val);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let result = 1;
+	for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
