87905e620334e2a662e5ddf82d3e4f660ded069a
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-02 11:16:35 +0800

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const properNum = Number(num); 
 
+    if (properNum === 0) return 0;
+    else if (properNum < 0) return "OOPS";
+
+    let currentResult = 1,
+        prevResult = 0;
+
+    for (let i = 1; i < properNum; i++) {
+        const temp = currentResult;
+        currentResult += prevResult;
+        prevResult = temp;
+    }
+
+    return currentResult;
 };
 
 // Do not edit below this line
