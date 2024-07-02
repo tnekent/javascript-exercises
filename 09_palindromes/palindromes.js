801c6fb85c23259ed8dcb600b4b6450be0385df1
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-02 10:47:03 +0800

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let origString = input.toLowerCase();
+    
+    const origStringArray = origString.split(""),
+        filteredStringArray = origStringArray.filter(char => {
+            const charCode = char.charCodeAt(0)
+            return (charCode > 47 && charCode < 58) || (charCode > 96 && charCode < 123); 
+        }),
+        reversedString = filteredStringArray.toReversed().join("");
 
+    origString = filteredStringArray.join("");
+    return reversedString === origString;
 };
 
 // Do not edit below this line
