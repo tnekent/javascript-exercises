3222a8f9c8a9499b2ff7864a7366c94a4dbc63d8
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-02 13:59:24 +0800

    Add solution for 12_findTheOldest
    Have two seperate solutions: using `array.toSorted()` and
    `array.reduce()`

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    const currentYear = new Date().getFullYear();
+    
+    // const sortedPeopleByDescAge = peopleList.toSorted((p1, p2) => {
+    //     // if yearOfDeath doesn't exist (assumably still alive), use current year
+    //     return ((p2.yearOfDeath || currentYear) - p2.yearOfBirth) -
+    //     ((p1.yearOfDeath || currentYear) - p1.yearOfBirth)
+    // })
+    // return sortedPeopleByDescAge[0];
 
+    const getAge = (person) =>
+        (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    return peopleList.reduce((oldestPerson, currentPerson) =>
+        getAge(oldestPerson) > getAge(currentPerson) ?
+        oldestPerson : currentPerson);
 };
 
 // Do not edit below this line
